5310a35e267ed35359482e9bf0f9fc8eb54ff513
Matthew Grove <matthew@grove.gq>
2019-02-05 07:25:39 +0000

    Remove precompiled _site folder

--- _site/computer-science-blog/js/css-presentation-slide.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    var year = new Date().getFullYear();
-    document.getElementById("css_attribute_list_link").innerHTML = 'All of the CSS style attributes are listed <a href="https://www.w3.org/TR/css-' + year + '/#properties" target="_blank">here</a>.';
-}
